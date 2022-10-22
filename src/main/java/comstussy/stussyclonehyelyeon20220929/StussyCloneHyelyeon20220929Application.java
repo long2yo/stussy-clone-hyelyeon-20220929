45be38d3bc7592beaba54ad93db29358f5031ec6
long2yo <long2yo@naver.com>
2022-10-22 17:13:19 +0900

    stussyCloneCoding  시작

---
+++ src/main/java/comstussy/stussyclonehyelyeon20220929/StussyCloneHyelyeon20220929Application.java
@@ -0,0 +1,13 @@
+package comstussy.stussyclonehyelyeon20220929;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyCloneHyelyeon20220929Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyCloneHyelyeon20220929Application.class, args);
+	}
+
+}
---
+++ src/test/java/comstussy/stussyclonehyelyeon20220929/StussyCloneHyelyeon20220929ApplicationTests.java
@@ -0,0 +1,13 @@
+package comstussy.stussyclonehyelyeon20220929;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyCloneHyelyeon20220929ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
